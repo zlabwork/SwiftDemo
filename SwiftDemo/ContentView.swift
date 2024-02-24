0f9927968947748e7c711a8c3d1f50c72e3f0942
Joe <joe@xxtime.com>
2024-02-25 00:45:46 +0800

    Initial Commit

---
+++ SwiftDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by Joe on 2024/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by Joe on 2024/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
