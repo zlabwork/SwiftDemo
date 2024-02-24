fc61d120b9dc3c0f52396e3ab24173fe25852e26
Joe <joe@xxtime.com>
2024-02-25 01:24:39 +0800

    read json file

---
+++ SwiftDemo/Case/ReadJsonFile.swift
@@ -0,0 +1,43 @@
+//
+//  ReadJsonFile.swift
+//  SwiftDemo
+//
+//  Created by Joe on 2024/2/25.
+//
+
+import Foundation
+
+class Dir {
+    
+    func readJsonFile(filename: String) -> Data {
+        guard let path = Bundle.main.path(forResource: filename, ofType: "json") else { return Data() }
+        do{
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            return data
+        } catch{
+            return Data()
+        }
+    }
+    
+    func loadJson<T: Decodable>(_ filename: String) -> T {
+        let data: Data
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        }
+    }
+}
